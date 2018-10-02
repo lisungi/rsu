31512d35564437e8a410ecc0125d0bb7b0d74885
lisungi <lisungidev@gmail.com>
2018-10-02 08:22:09 +0100

    Commit tools-full-questionnaire branch

---
+++ tools/full-questionnaire.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en" class="default-style">
+
+<head>
+  <title><?= SITE_NAME . ':' . strtoupper(e($thisPath[2])); ?></title>
+
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
+
+  <!-- Icon fonts -->
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/fonts/fontawesome.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/fonts/ionicons.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/fonts/linearicons.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/fonts/open-iconic.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/fonts/pe-icon-7-stroke.css">
+
+  <!-- Core stylesheets -->
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/css/rtl/bootstrap.css" class="theme-settings-bootstrap-css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/css/rtl/appwork.css" class="theme-settings-appwork-css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/css/rtl/theme-corporate.css" class="theme-settings-theme-css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/css/rtl/colors.css" class="theme-settings-colors-css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/css/rtl/uikit.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/css/pages/demo.css">
+
+  <script src="<?= CDN; ?>_vendor/js/material-ripple.js"></script>
+  <script src="<?= CDN; ?>_vendor/js/layout-helpers.js"></script>
+
+  <!-- Core scripts -->
+  <script src="<?= CDN; ?>_vendor/js/pace.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+
+  <!-- Libs -->
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+  <link rel="stylesheet" href="<?= CDN; ?>_vendor/libs/datatables/datatables.css">
+
+  <script src="https://d3js.org/d3.v4.min.js"></script>
+  <script src="<?= CDN; ?>_tools/n.js"></script>
+</head>
+
+<body>
+  <div class="page-loader">
+    <div class="bg-primary"></div>
+  </div>
+
+  <!-- Core scripts -->
+  <script src="<?= CDN; ?>_vendor/js/jquery-3.3.1.min.js"></script>
+  <script src="<?= CDN; ?>_vendor/libs/popper/popper.js"></script>
+  <script src="<?= CDN; ?>_vendor/js/bootstrap.js"></script>
+  <script src="<?= CDN; ?>_vendor/js/sidenav.js"></script>
+
+<!-- Libs -->
+<script src="<?= CDN; ?>_vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="<?= CDN; ?>_vendor/libs/chartjs/chartjs.js"></script>
+
+
+</body>
+
+</html>
